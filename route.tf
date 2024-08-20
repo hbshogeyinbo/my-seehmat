c08540a1bedc817aefffcabdfb31944951caa473
hameed <hameedshogeyinbo@gmail.com>
2024-08-19 22:37:49 -0300

    updated all hardcoded files

--- route.tf
+++ route.tf
@@ -1,34 +1,29 @@
-# route53.tf
+# route.tf
 
-# Define a variable for the domain name
-variable "domain_name" {
-  description = "The domain name for which to fetch the ACM certificate."
-  type        = string
-  default     = "seehmat.com"  # Adjust as necessary
-}
+# provider "aws" {
+#   alias  = "eu"
+#   region = var.aws_region
+# }
 
-# Route 53 Hosted Zone
+# provider "aws" {
+#   alias  = "us"
+#   region = var.aws_us_region
+# }
+
 resource "aws_route53_zone" "seehmat_zone" {
-  name = "seehmat.com"
-}
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
+  name = var.domain_name
 }
 
-# Fetch ACM Certificate ARN dynamically
 data "aws_acm_certificate" "seehmat_cert" {
   domain   = var.domain_name
   statuses = ["ISSUED"]
   most_recent = true
-  provider  = aws.us_east_1
-  
+  provider  = aws.us
 }
 
-# Route 53 A Record (Alias) for Root Domain
 resource "aws_route53_record" "root_domain" {
   zone_id = aws_route53_zone.seehmat_zone.zone_id
-  name    = "seehmat.com"
+  name    = var.domain_name
   type    = "A"
 
   alias {
@@ -38,12 +33,10 @@ resource "aws_route53_record" "root_domain" {
   }
 }
 
-# Route 53 CNAME Record for www Subdomain
 resource "aws_route53_record" "www_subdomain" {
   zone_id = aws_route53_zone.seehmat_zone.zone_id
-  name    = "www.seehmat.com"
+  name    = "www.${var.domain_name}"
   type    = "CNAME"
   ttl     = 300
   records = [aws_cloudfront_distribution.s3_distribution.domain_name]
 }
-
