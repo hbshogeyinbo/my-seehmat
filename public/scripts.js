12545fb1d80f81154c09a31e62357e5449ffbad1
hameed <hameedshogeyinbo@gmail.com>
2024-09-23 10:46:21 -0300

    Visitor count

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
+// Visitor counter code
+document.addEventListener('DOMContentLoaded', async function() {
+    // URL of the API Gateway (use the correct URL for your API)
+    const apiUrl = 'https://dtchx90qob.execute-api.eu-west-1.amazonaws.com/dev';
+
+    // Function to send a POST request to the API Gateway
+    async function incrementVisitorCount() {
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',  // HTTP method
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Visitor Count Incremented:', data);  // Log success
+            } else {
+                console.error('Error:', response.statusText);  // Handle errors
+            }
+        } catch (error) {
+            console.error('Error:', error);  // Handle network errors
+        }
+    }
+
+    // Call incrementVisitorCount when the page loads
+    incrementVisitorCount();
+
+    // Menu functionality code
     const menuItems = document.querySelectorAll('.menu div');
 
     menuItems.forEach(item => {
