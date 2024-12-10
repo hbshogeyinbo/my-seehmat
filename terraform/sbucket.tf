ff0321e6cfe70ea0b519214fb9f0b05a1c39b5a7
hameed <hameedshogeyinbo@gmail.com>
2024-12-09 21:44:21 -0400

    new work

--- terraform/sbucket.tf
+++ terraform/sbucket.tf
@@ -129,3 +129,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     Environment = "Production"
   }
 }
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
