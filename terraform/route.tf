7794850b995c6540ed9ea10c7351ed076379faa9
hameed <hameedshogeyinbo@gmail.com>
2024-12-13 19:19:56 -0400

    updated all files

--- terraform/route.tf
+++ terraform/route.tf
@@ -47,16 +47,3 @@ resource "aws_route53_record" "www_subdomain" {
   ttl     = 300
   records = [aws_cloudfront_distribution.s3_distribution.domain_name]
 }
-
-# Create an Alias Record for CloudFront
-resource "aws_route53_record" "cloudfront_alias" {
-  zone_id = aws_route53_zone.seehmat_new.id
-  name    = var.domain_name
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
