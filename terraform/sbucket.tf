f6c52abdbee85aef46c968fc49c6f075f1cf276c
hameed <hameedshogeyinbo@gmail.com>
2024-12-21 08:42:24 -0400

    new website look

--- terraform/sbucket.tf
+++ terraform/sbucket.tf
@@ -136,7 +136,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 }
 
-# Outputs
-output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.s3_distribution.id
-}
+# # Outputs
+# output "cloudfront_distribution_id" {
+#   value = aws_cloudfront_distribution.s3_distribution.id
+# }
